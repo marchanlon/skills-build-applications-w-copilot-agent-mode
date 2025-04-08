505b25713cfe8b6e60274e3e240ce53da71cf22e
Marc Hanlon <marchanlon@gmail.com>
2025-04-08 16:24:31 +0000

    Always use full URL

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = f'{request.scheme}://{request.get_host()}/'
+    base_url = f'https://organic-space-dollop-wrwrp566xp9fvj-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
